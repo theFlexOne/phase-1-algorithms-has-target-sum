05675316b52d38febf18ad33d467a4accfc0ff4f
Eric Hafvenstein <edhavey@null.net>
2021-10-11 16:24:30 -0500

    formatted my notes before submission

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function hasTargetSum(array, winningSum) {
   const previousNums = {};
   for (let i = 0; i < array.length; i++) {
     const currentNum = array[i];
-    const winningNum = winningSum - currentNum;
-    if (winningNum in previousNums) return true;
+    const NumToWin = winningSum - currentNum;
+    if (NumToWin in previousNums) return true;
     previousNums[currentNum] = i;
   }
   return false;
@@ -15,22 +15,21 @@ function hasTargetSum(array, winningSum) {
 
 /* 
   pseudo-code:
-  A) 
+  A) create an object to act as a history of previous numbers.
+  B) Loop through the array checking if each number has a winning pair in the history object
+  C) if there is one, return true
+  D) if not, end the iteration and 
+  E) will return false if loop finishes before returning.
 */
 
 /*
   I've seen a YouTube video, or two, on this problem, and others, so I'm aware that using an object to
-    record previous iteration data is a fast solution. I'm under the impression that, unless there's
-    no other way around it, a time complexity of O of N^2 is bad. Even O of log(N^2) is considered way more acceptable.
-    But, trying the brute force method is a good place to start, if no other solutions come to mind.
+    record previous iteration data is a fast solution, but, trying the brute force method is a good place to start, if no other solutions come to mind.
 
   The explanation is pretty simple, I created an object to record previous iterations with dynamic key:value pairs,
-    and checked to see if the number needed for the pair to equal the target was in the object each iteration.
+    and checked to see if the number needed for the pair to equal the target sum was in the object each iteration.
   If the number was in the object, I immediately returned true and stopped iterating, but if the loop came to an end,
     it meant that there were no pairs with a sum that equalled the target, so I returned false.
-
-  I also added a guard clause that immediately continued the for loop if the current number was greater than or 
-    equal to the target sum, to technically save some time, I think. Still a time complexity of O of N.
 */
 
 // You can run `node index.js` to view these console logs
